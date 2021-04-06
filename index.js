ef6ba9bcc0f34098e2578fd22e63d781755374b7
jinook929 <jinook929@gmail.com>
2021-04-06 09:40:24 -0400

    commit tree 02cbb6e76d42552f02e2fffe69361ec5c17527a7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event_name) {
+  const arr = []
+  for(let i = 0; i < names.length; i++) {
+    arr.push(`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`)
+  }
+  return arr
+}
+
+function countDown(num) {
+  // let counter = 0
+  while(num >= 0) {
+    console.log(num)
+    num--
+  }
+}
